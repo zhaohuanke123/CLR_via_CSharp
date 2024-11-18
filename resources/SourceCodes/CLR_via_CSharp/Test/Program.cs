058293d12da352a078f754fc32e1afdd838b680b
vanko <1728931104@qq.com>
2024-11-18 18:08:21 +0800

    添加练习struct readonly

--- resources/SourceCodes/CLR_via_CSharp/Test/Program.cs
+++ resources/SourceCodes/CLR_via_CSharp/Test/Program.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-
-namespace Test
+﻿namespace Test
 {
     internal class Program
     {
-
-        static void Main(string[] args)
+        public static void Main()
         {
-            
         }
     }
 }
---
+++ resources/SourceCodes/CLR_via_CSharp/TestStruct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestStruct
+{
+    internal class Program
+    {
+        struct MyStruct
+        {
+            public int a;
+
+            public void Increment()
+            {
+                a++;
+            }
+
+            public MyStruct(int a)
+            {
+                this.a = a;
+            }
+        }
+
+        static void Main()
+        {
+            List<MyStruct> list = new List<MyStruct>();
+            list.Add(new MyStruct(10));
+            Console.WriteLine(list[0].a);
+        }
+    }
+}
