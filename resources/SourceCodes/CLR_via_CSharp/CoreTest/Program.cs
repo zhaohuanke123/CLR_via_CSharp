db3e3dca163941493e4cacd67986b71879000d84
JoVanko <1728931104@qq.com>
2024-11-25 17:00:25 +0800

    下午休息一下

--- resources/SourceCodes/CLR_via_CSharp/CoreTest/Program.cs
+++ resources/SourceCodes/CLR_via_CSharp/CoreTest/Program.cs
@@ -2,10 +2,47 @@
 
 class Program
 {
+    struct MyStruct
+    {
+        public int a;
+        //public int b;
+        //public int c;
+        //public int d;
+        //public int e;
+        //public int f;
+        //public int g;
+        //public int h;
+        //public int i;
+
+
+        public MyStruct(int a)
+        {
+            this.a = a;
+            //this.b = a;
+            //this.c = a;
+            //this.d = a;
+            //this.e = a;
+            //this.f = a;
+            //this.g = a;
+            //this.h = a;
+            //this.i = a;
+        }
+    }
+
     static void Main(string[] args)
     {
-        string s = "hehehehe";
-        string s1 = new string('h', 5);
-        Console.WriteLine(s + s1);
+        MyStruct myStruct = new MyStruct();
+
+        Console.WriteLine(myStruct);
+
+        MyStruct myStruct1 = new MyStruct(1);
+        Console.WriteLine(myStruct1);
+
+        MyStruct ms = new MyStruct();
+
+        Console.WriteLine(ms);
+        MyStruct m1 = new MyStruct();
+
+        Console.WriteLine(m1);
     }
 }
