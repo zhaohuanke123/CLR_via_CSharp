31f736dd3fc906c2e8a574e2827dceb29b023d38
JoVanko <1728931104@qq.com>
2024-12-06 19:04:43 +0800

    note 13-17

---
+++ resources/SourceCodes/CLR_via_CSharp/TestInterfaceDerive/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestInterfaceDerive
+{
+    interface IA
+    {
+        void Method();
+    }
+
+    interface IB : IA
+    {
+        new void Method();
+    }
+
+    class MyClass : IB
+    {
+        public void Method()
+        {
+            Console.WriteLine("MyClass.Method()");
+        }
+        // void IA.Method()
+        // {
+        //     
+        // }
+        //
+        // void IB.Method()
+        // {
+        // }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            IA ia = mc;
+            IB ib = mc;
+            ia.Method();
+            ib.Method();
+        }
+    }
+}
