571d8dd67b46effee84435fd82f8f54ef9166ad0
JoVanko <1728931104@qq.com>
2024-12-08 09:15:34 +0800

    补充错题

---
+++ resources/SourceCodes/CLR_via_CSharp/TestStringFormat/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestStringFormat
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			string.Format("{1}{2}{3}", 1, 2, 3);
+			// string.Format("{0}{1}{2}{3}", 1, 2, 3, 4, 5);
+		}
+	}
+}
