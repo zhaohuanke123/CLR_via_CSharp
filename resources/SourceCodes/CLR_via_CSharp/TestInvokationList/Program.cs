b507863cef6cfd1322b435c48ccd319f5213e67b
JoVanko <1728931104@qq.com>
2024-12-10 10:16:34 +0800

    测试委托链

---
+++ resources/SourceCodes/CLR_via_CSharp/TestInvokationList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestInvokationList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Action a = () => { };
+            Action b = null;
+            b += a;
+            b += a;
+            b += a;
+
+            foreach (var dele in b.GetInvocationList())
+            {
+                Console.WriteLine(ReferenceEquals(dele, a));
+            }
+        }
+    }
+}
