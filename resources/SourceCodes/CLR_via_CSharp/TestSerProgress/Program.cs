6cf60856dc041cfb2712761e23bf115bfc3e0f01
JoVanko <1728931104@qq.com>
2024-12-23 19:49:02 +0800

    1 序列化错题

---
+++ resources/SourceCodes/CLR_via_CSharp/TestSerProgress/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TestSerProgress
+{
+    [Serializable]
+    class MyClass
+    {
+        private int a;
+        private string b;
+        private MyClass2 MyClass2;
+    }
+
+    class MyClass2
+    {
+        private MyClass2 _myClass2;
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var members = FormatterServices.GetSerializableMembers(typeof(MyClass));
+            foreach (var info in members)
+            {
+                Console.WriteLine(info.GetType());
+            }
+            // FormatterServices.GetTypeFromAssembly()
+        }
+    }
+}
---
+++ resources/SourceCodes/CLR_via_CSharp/TestThreadPro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestThreadPro
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
