0073087d87d5eefb4232d2ace27558a3a7c17b9b
JoVanko <1728931104@qq.com>
2024-12-24 18:15:45 +0800

    CLR 最终错题

---
+++ resources/SourceCodes/CLR_via_CSharp/TestSerList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TestSerList
+{
+    [Serializable]
+    internal class Test : ISerializable
+    {
+        private Test2 test2;
+        Test2[] list = new Test2[3];
+
+        public Test()
+        {
+            test2 = new Test2();
+            list[0] = new Test2();
+            list[1] = new Test2();
+            list[2] = new Test2();
+        }
+
+        private Test(SerializationInfo info, StreamingContext context)
+        {
+            test2 = (Test2)info.GetValue("test2", typeof(Test2));
+            list = (Test2[])info.GetValue("list", typeof(Test2[]));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("test2", test2);
+            info.AddValue("list", list);
+        }
+
+        public override string ToString()
+        {
+            string s = "test2: " + test2 + "\n";
+            s += "list: ";
+            foreach (Test2 t in list)
+            {
+                s += t + " ";
+            }
+
+            return s;
+        }
+    }
+
+    [Serializable]
+    internal class Test2
+    {
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test t = new Test();
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream("test.dat", FileMode.Create))
+            {
+                bf.Serialize(fs, t);
+                fs.Position = 0;
+                Test t2 = (Test)bf.Deserialize(fs);
+                Console.Write(t2);
+            }
+        }
+    }
+}
---
+++ resources/SourceCodes/CLR_via_CSharp/TestSerSelector/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace TestSerSelector
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters.Binary;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // 创建并配置代理选择器
+            SurrogateSelector selector1 = new SurrogateSelector();
+            SurrogateSelector selector2 = new SurrogateSelector();
+
+            // 在 selector1 中添加代理，用于序列化 MyType
+            selector1.AddSurrogate(typeof(MyType), new StreamingContext(StreamingContextStates.All),
+                new MyTypeSurrogate());
+
+            // 在 selector2 中添加代理，用于序列化 MyTypeV2
+            selector2.AddSurrogate(typeof(MyTypeV2), new StreamingContext(StreamingContextStates.All),
+                new MyTypeV2Surrogate());
+
+            // 链接两个选择器
+            selector1.ChainSelector(selector2);
+
+            // 创建一个对象并序列化
+            MyType myTypeObj = new MyType { MyValue = 42 };
+            byte[] serializedData = SerializeObject(myTypeObj, selector1);
+
+            // 反序列化并输出结果
+            MyType deserializedObj = (MyType)DeserializeObject(serializedData, selector1);
+            Console.WriteLine($"Deserialized MyTypeV2: MyValue = {deserializedObj.MyValue}");
+        }
+
+        // 序列化方法
+        public static byte[] SerializeObject(object obj, SurrogateSelector selector)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.SurrogateSelector = selector;
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        // 反序列化方法
+        public static object DeserializeObject(byte[] serializedData, SurrogateSelector selector)
+        {
+            using (MemoryStream stream = new MemoryStream(serializedData))
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.SurrogateSelector = selector;
+                return formatter.Deserialize(stream);
+            }
+        }
+    }
+
+// 原始类型 MyType
+    [Serializable]
+    public class MyType
+    {
+        public int MyValue { get; set; }
+    }
+
+// 版本 2 类型 MyTypeV2
+    [Serializable]
+    public class MyTypeV2
+    {
+        public int MyValue { get; set; }
+    }
+
+// MyType 的序列化代理
+    public class MyTypeSurrogate : ISerializationSurrogate
+    {
+        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+        {
+            MyType myType = (MyType)obj;
+            info.AddValue("MyValue", myType.MyValue);
+        }
+
+        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context,
+            ISurrogateSelector selector)
+        {
+            MyType myType = (MyType)obj;
+            myType.MyValue = info.GetInt32("MyValue");
+            return myType;
+        }
+    }
+
+// MyTypeV2 的序列化代理
+    public class MyTypeV2Surrogate : ISerializationSurrogate
+    {
+        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+        {
+            MyTypeV2 myTypeV2 = (MyTypeV2)obj;
+            info.AddValue("MyValue", myTypeV2.MyValue);
+        }
+
+        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context,
+            ISurrogateSelector selector)
+        {
+            MyTypeV2 myTypeV2 = (MyTypeV2)obj;
+            myTypeV2.MyValue = info.GetInt32("MyValue");
+            return myTypeV2;
+        }
+    }
+}
