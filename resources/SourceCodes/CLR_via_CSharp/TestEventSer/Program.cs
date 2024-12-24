942dcd79b78e5e82443a065f8dca9e91433effc5
JoVanko <1728931104@qq.com>
2024-12-24 11:13:34 +0800

    序列化 事件

---
+++ resources/SourceCodes/CLR_via_CSharp/TestEventSer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TestEventSer
+{
+    [Serializable]
+    class Test
+    {
+        public event Action<int> callBack;
+
+        public void Call(int i)
+        {
+            callBack?.Invoke(i);
+        }
+    }
+
+    [Serializable]
+    class Test2
+    {
+        public void TestInstance(int i)
+        {
+            Console.WriteLine(i + " " + this);
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var test = new Test();
+            var test2 = new Test2();
+            // test.callBack += test2.TestInstance;
+            // test.callBack += test2.TestInstance;
+            // test.Call(1);
+            Action<int> action = test2.TestInstance;
+
+            var binaryFormatter = new BinaryFormatter();
+            var stream = new FileStream("data.txt", FileMode.Create);
+
+            binaryFormatter.Serialize(stream, action);
+            stream.Position = 0;
+            // stream.Close();
+            var deserialize = binaryFormatter.Deserialize(stream) as Action<int>;
+            deserialize.Invoke(1);
+            // deserialize.Call(1);
+        }
+    }
+}
--- resources/SourceCodes/CLR_via_CSharp/TestSerProgress/Program.cs
+++ resources/SourceCodes/CLR_via_CSharp/TestSerProgress/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices.ComTypes;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
@@ -12,8 +12,11 @@ namespace TestSerProgress
         private int a;
         private string b;
         private MyClass2 MyClass2;
+        private const int t = 1;
+        public List<object> List = new List<object>();
     }
 
+    //[Serializable]
     class MyClass2
     {
         private MyClass2 _myClass2;
@@ -23,11 +26,23 @@ namespace TestSerProgress
     {
         public static void Main(string[] args)
         {
-            var members = FormatterServices.GetSerializableMembers(typeof(MyClass));
-            foreach (var info in members)
+            var myClass = new MyClass();
+            myClass.List.Add(new MyClass2());
+            myClass.List.Add(new MyClass2());
+            myClass.List.Add(new MyClass2());
+
+            var serializableMembers = FormatterServices.GetSerializableMembers(typeof(MyClass));
+            foreach (var member in serializableMembers)
             {
-                Console.WriteLine(info.GetType());
+                Console.WriteLine(member);
             }
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            var memoryStream = new MemoryStream();
+            binaryFormatter.Serialize(memoryStream, new MyClass());
+            memoryStream.Position = 0;
+            var deserialize = binaryFormatter.Deserialize(memoryStream);
+            Console.WriteLine(deserialize);
+
             // FormatterServices.GetTypeFromAssembly()
         }
     }
