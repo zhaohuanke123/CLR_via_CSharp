d72204b2fa7d9ee55d9f399a83b389e1d07ff8e1
JoVanko <1728931104@qq.com>
2025-01-19 14:46:49 +0800

    1

---
+++ resources/SourceCodes/CLR_via_CSharp/TestTaskException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace TestTaskException
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Task t1 = Task.Run(() => { throw new ArgumentException("Task exception"); });
+                Task t2 = Task.Run(() => { throw new DataException("Data exception"); });
+                Task t3 = Task.Run(() => { throw new InvalidOperationException("Invalid operation exception"); });
+
+                Task.WaitAll(t1, t2, t3);
+            }
+            catch (AggregateException e)
+            {
+                Console.Out.WriteLine(e.InnerExceptions.Count);
+                foreach (Exception innerException in e.InnerExceptions)
+                {
+                    Console.Out.WriteLine("Exception: " + innerException);
+                }
+
+                Console.Out.WriteLine(e.InnerException);
+            }
+        }
+    }
+}
