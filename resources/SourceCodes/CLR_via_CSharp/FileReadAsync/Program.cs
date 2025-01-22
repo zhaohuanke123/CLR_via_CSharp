ab889c633612c528391e7797c14778d8aefb1ac9
JoVanko <1728931104@qq.com>
2025-01-22 19:17:53 +0800

    测试 异步IO

---
+++ resources/SourceCodes/CLR_via_CSharp/FileReadAsync/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileReadAsync
+{
+    internal class Program
+    {
+        private static object o;
+
+        public static async Task Main(string[] args)
+        {
+            // 写一个异步文件读取
+            string filePath = @"C:\Users\zhaohuanke\Desktop\Test.txt";
+
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+
+            byte[] bytes = new byte[fs.Length];
+            var task = fs.ReadAsync(bytes, 0, bytes.Length);
+
+            var res = await task;
+            var res1 = await task;
+            var res2 = await task;
+            var res3 = await task;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var res4 = await task;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                var res5 = await task;
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ resources/SourceCodes/CLR_via_CSharp/FileReadAsync/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FileReadAsync")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileReadAsync")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("A917FE66-99D3-4397-BFB2-5A85FA41DD30")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
